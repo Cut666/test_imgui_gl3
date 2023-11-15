b27d73ad2c65d21f5e8523cdc507a01b0550bddc
Cut666 <110405911+Cut666@users.noreply.github.com>
2023-11-15 09:53:25 +0700

    Add files via upload

---
+++ imgui_demo1/src/main.cpp
@@ -0,0 +1,286 @@
+// Dear ImGui: standalone example application for GLFW + OpenGL 3, using programmable pipeline
+// (GLFW is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
+
+// Learn about Dear ImGui:
+// - FAQ                  https://dearimgui.com/faq
+// - Getting Started      https://dearimgui.com/getting-started
+// - Documentation        https://dearimgui.com/docs (same as your local docs/ folder).
+// - Introduction, links and more at the top of imgui.cpp
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <stdio.h>
+#define GL_SILENCE_DEPRECATION
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+#endif
+#include <GLFW/glfw3.h> // Will drag system OpenGL headers
+
+// [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
+// To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
+// Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
+
+// This example can also compile and run with Emscripten! See 'Makefile.emscripten' for details.
+#ifdef __EMSCRIPTEN__
+#include "../libs/emscripten/emscripten_mainloop_stub.h"
+#endif
+
+static void glfw_error_callback(int error, const char* description)
+{
+    fprintf(stderr, "GLFW Error %d: %s\n", error, description);
+}
+
+// Main code
+int main(int, char**)
+{
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit())
+        return 1;
+
+    // Decide GL+GLSL versions
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+    // GL ES 2.0 + GLSL 100
+    const char* glsl_version = "#version 100";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+#elif defined(__APPLE__)
+    // GL 3.2 + GLSL 150
+    const char* glsl_version = "#version 150";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+#else
+    // GL 3.0 + GLSL 130
+    const char* glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+#endif
+
+    // Create window with graphics context
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL3 example", nullptr, nullptr);
+    if (window == nullptr)
+        return 1;
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsLight();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    // Load Fonts
+    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
+    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
+    // - If the file cannot be loaded, the function will return a nullptr. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+    // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use Freetype for higher quality font rendering.
+    // - Read 'docs/FONTS.md' for more instructions and details.
+    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+    // - Our Emscripten build process allows embedding fonts to be accessible at runtime from the "fonts/" folder. See Makefile.emscripten for details.
+    //io.Fonts->AddFontDefault();
+    //io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf", 18.0f);
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
+    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, nullptr, io.Fonts->GetGlyphRangesJapanese());
+    //IM_ASSERT(font != nullptr);
+    io.Fonts->AddFontDefault();
+    ImFont* font1 = io.Fonts->AddFontFromFileTTF("/usr/share/fonts/truetype/fonts-gujr-extra/aakar-medium.ttf", 30.0f, nullptr, io.Fonts->GetGlyphRangesVietnamese());
+    ImFont* font2 = io.Fonts->AddFontFromFileTTF("/usr/share/fonts/truetype/fonts-gujr-extra/aakar-medium.ttf", 25.0f, nullptr, io.Fonts->GetGlyphRangesVietnamese());
+    ImFont* font3 = io.Fonts->AddFontFromFileTTF("/usr/share/fonts/truetype/fonts-gujr-extra/aakar-medium.ttf", 20.0f, nullptr, io.Fonts->GetGlyphRangesVietnamese());
+    IM_ASSERT(font1 != nullptr);
+    IM_ASSERT(font2 != nullptr);
+    IM_ASSERT(font3 != nullptr);
+
+    // Our state
+    /*bool show_demo_window = true;
+    bool show_another_window = false;*/
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    // Main loop
+#ifdef __EMSCRIPTEN__
+    // For an Emscripten build we are disabling file-system access, so let's not attempt to do a fopen() of the imgui.ini file.
+    // You may manually call LoadIniSettingsFromMemory() to load settings from your own storage.
+    io.IniFilename = nullptr;
+    EMSCRIPTEN_MAINLOOP_BEGIN
+#else
+    while (!glfwWindowShouldClose(window))
+#endif
+    {
+        // Poll and handle events (inputs, window resize, etc.)
+        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
+        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
+        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+        glfwPollEvents();
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+       /* if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);*/
+
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+        //{
+        //    static float f = 0.0f;
+        //    static int counter = 0;
+
+            ImGui::Begin("User Interface");                          // Create a window called "Hello, world!" and append into it.
+            ImGui::GetStyle().Colors[ImGuiCol_WindowBg] = ImVec4(193.0f / 255.0f, 228.0f / 255.0f, 205.0f / 255.0f, 1.0f);
+            ImGui::PushFont(font1);
+            ImGui::TextColored(ImVec4(38.0f / 255.0f, 95.0f / 255.0f, 42.0f / 255.0f, 1.0f), "Appeal Application Form");
+            ImGui::PopFont();
+            ImGui::PushFont(font2);
+            ImGui::TextColored(ImVec4(100.0f / 255.0f, 160.0f / 255.0f, 112.0f / 255.0f, 1.0f), "Personal Information");
+            ImGui::PopFont();
+            float cornerRounding = 5.0f; // Độ bo góc mong muốn
+
+            ImGui::PushStyleVar(ImGuiStyleVar_FrameRounding, cornerRounding);
+            ImGui::PushFont(font3);
+            ImGui::TextColored(ImVec4(100.0f / 255.0f, 160.0f / 255.0f, 112.0f / 255.0f, 1.0f), "Name");
+
+            //char inputBufferfirt[256] = "";
+            //char inputBufferlast[256] = "";// Để lưu trữ giá trị nhập vào
+            ImVec4 bgColor = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);
+            ImGui::PushStyleColor(ImGuiCol_FrameBg, bgColor);
+            ImVec4 borderColor = ImVec4(0.0f, 0.0f, 0.0f, 1.0f);
+            ImGui::PushStyleColor(ImGuiCol_Border, borderColor);
+            ImVec4 textColor = ImVec4(100.0f / 255.0f, 160.0f / 255.0f, 112.0f / 255.0f, 1.0f);
+            ImGui::PushStyleColor(ImGuiCol_Text, textColor);
+
+            ImGui::Columns(2, "InputColumns", true);
+
+            // Đặt chiều rộng của cột 1 thành 50% (0.5f) chiều rộng hiện tại
+            ImGui::SetColumnWidth(0, ImGui::GetWindowWidth() * 0.5f);
+
+            // Ô nhập dữ liệu - Cột 1
+            static char inputBuffer1[256] = ""; // Để lưu trữ giá trị nhập vào
+            ImGui::SetNextItemWidth(-1.0f);
+            ImGui::InputText("##InputText1", inputBuffer1, sizeof(inputBuffer1));
+            // Ô nhập dữ liệu - Cột 2
+            static char inputBuffer2[256] = ""; // Để lưu trữ giá trị nhập vào
+
+            ImGui::NextColumn(); // Di chuyển đến cột tiếp theo
+            ImGui::SetNextItemWidth(-1.0f);
+            ImGui::InputText("##InputText2", inputBuffer2, sizeof(inputBuffer2));
+
+            // Kết thúc layout cột
+            ImGui::Columns(1);
+
+            ImGui::PopStyleColor(3);
+            ImGui::PopFont();
+
+            ImGui::PushFont(font3);
+            ImGui::TextColored(ImVec4(100.0f / 255.0f, 160.0f / 255.0f, 112.0f / 255.0f, 1.0f), "Student Number");
+            static char inputBuffernumber[256] = ""; // Để lưu trữ giá trị nhập vào
+            ImGui::SetNextItemWidth(-1.0f);
+            ImVec4 bgColor1 = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);
+            ImGui::PushStyleColor(ImGuiCol_FrameBg, bgColor1);
+            ImVec4 borderColor1 = ImVec4(0.0f, 0.0f, 0.0f, 1.0f);
+            ImGui::PushStyleColor(ImGuiCol_Border, borderColor1);
+            ImVec4 textColor1 = ImVec4(100.0f / 255.0f, 160.0f / 255.0f, 112.0f / 255.0f, 1.0f);
+            ImGui::PushStyleColor(ImGuiCol_Text, textColor1);
+            ImGui::InputText("##InputText3", inputBuffernumber, sizeof(inputBuffernumber));
+            ImGui::PopStyleColor(3);
+            ImGui::PopFont();
+
+            ImGui::PushFont(font2);
+            ImGui::TextColored(ImVec4(100.0f / 255.0f, 160.0f / 255.0f, 112.0f / 255.0f, 1.0f), "Appeal Information");
+            ImGui::PopFont();
+
+            ImGui::PushFont(font3);
+            ImGui::TextColored(ImVec4(100.0f / 255.0f, 160.0f / 255.0f, 112.0f / 255.0f, 1.0f), "Request Summary");
+            static char inputBufferSummary[256] = "";
+            static char inputBufferUpload[256] = "";// Để lưu trữ giá trị nhập vào
+            ImGui::SetNextItemWidth(-1.0f);
+            ImVec4 bgColor2 = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);
+            ImGui::PushStyleColor(ImGuiCol_FrameBg, bgColor2);
+            ImVec4 borderColor2 = ImVec4(0.0f, 0.0f, 0.0f, 1.0f);
+            ImGui::PushStyleColor(ImGuiCol_Border, borderColor2);
+            ImVec4 textColor2 = ImVec4(100.0f / 255.0f, 160.0f / 255.0f, 112.0f / 255.0f, 1.0f);
+            ImGui::PushStyleColor(ImGuiCol_Text, textColor2);
+            ImGui::InputTextMultiline("##InputText4", inputBufferSummary, sizeof(inputBufferSummary), ImVec2(-1.0f, 90));
+
+            ImGui::TextColored(ImVec4(100.0f / 255.0f, 160.0f / 255.0f, 112.0f / 255.0f, 1.0f), "Letter of Appeal Upload");
+            ImGui::InputTextMultiline("##InputText5", inputBufferUpload, sizeof(inputBufferUpload), ImVec2(-1.0f, 60));
+
+            ImGui::TextColored(ImVec4(100.0f / 255.0f, 160.0f / 255.0f, 112.0f / 255.0f, 1.0f), "Do you have supporting documents to upload in support of your appeal?");
+            ImGui::SetNextItemWidth(-1.0f);
+            const char* items[] = { "Yes", "No" };
+            static int selectedItem = -1; // Chưa có mục nào được chọn mặc định
+            static const char* displayValue = ""; // Giá trị hiển thị mặc định
+
+            if (selectedItem == -1)
+                displayValue = "Yes";
+            else
+                displayValue = items[selectedItem];
+
+            if (ImGui::BeginCombo("##Combo", displayValue))
+            {
+                for (int i = 0; i < IM_ARRAYSIZE(items); i++)
+                {
+                    const bool isSelected = (selectedItem == i);
+                    if (ImGui::Selectable(items[i], isSelected))
+                    {
+                        selectedItem = i;
+                    }
+                    if (isSelected)
+                    {
+                        ImGui::SetItemDefaultFocus();
+                    }
+                }
+                ImGui::EndCombo();
+            }
+            ImGui::PopStyleColor(3);
+
+            ImGui::PopStyleVar();
+
+            ImGui::PopFont();
+            ImGui::End();
+
+        // Rendering
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+#ifdef __EMSCRIPTEN__
+    EMSCRIPTEN_MAINLOOP_END;
+#endif
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
